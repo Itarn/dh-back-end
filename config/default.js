cdc53c7b2c7660d8349df2d244d556380ceb10bc
AlanWang <690418095@qq.com>
2020-07-23 17:09:49 +0800

    Update default.js
    Update default.js

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@
 const config  = {
   host     : '127.0.0.1',   // 数据库地址
   user     : 'root',    // 数据库用户
-  password : '/180527*hh',   // 数据库密码
+  password : 'xx',   // 数据库密码
   database : 'dunhuang'  // 选中数据库
 }
 
